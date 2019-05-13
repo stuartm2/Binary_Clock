03793bc4e81fda0bd441a2beeccce73fba149639
Stuart Metcalfe <stuartm2@gmail.com>
2019-05-13 12:17:49 +0100

    Move initial .ino file to subdirectory

--- Main_Board/Main_Board.ino
+++ Main_Board/Main_Board.ino
@@ -1,6 +1,7 @@
 
 #include <Wire.h>
 #include "RTClib.h"
+#include <Adafruit_SSD1306.h>
 
 #define PIN_COL_A 8
 #define PIN_COL_B 7
@@ -12,20 +13,44 @@
 #define PIN_BTN_HRS 2
 #define PIN_BTN_MIN 3
 #define PIN_BTN_SEC 4
+#define PIN_SET_MODE 13
 #define PIN_BRT 5
 #define PIN_LIGHT_SENS A3
 
+#define MODE_SET_TIME = 1
+#define MODE_SET_DATE = 0
+
+const char* MONTHS[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+const char* ORDS[31] = {"st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th",
+                        "th", "th", "th", "th", "th", "th", "th", "th", "th", "th",
+                        "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th",
+                        "st"};
+
 int hrs = 0,
     mins = 0,
-    secs = 0;
+    secs = 0,
+    y = 2000,
+    m = 1,
+    d = 1;
 
 RTC_PCF8523 rtc;
+
+#define OLED_RESET 4
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 unsigned long lastUpdate = 0;
 unsigned long lastHrsPressed = 0;
 unsigned long lastMinsPressed = 0;
+unsigned long lastSecsPressed = 0;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.print("Starting ...\n");
+
   pinMode(PIN_COL_A, OUTPUT);
   pinMode(PIN_COL_B, OUTPUT);
   pinMode(PIN_COL_C, OUTPUT);
@@ -36,6 +61,7 @@ void setup() {
   pinMode(PIN_BTN_HRS, INPUT_PULLUP);
   pinMode(PIN_BTN_MIN, INPUT_PULLUP);
   pinMode(PIN_BTN_SEC, INPUT_PULLUP);
+  pinMode(PIN_SET_MODE, INPUT);
   pinMode(PIN_BRT, OUTPUT);
   
   if (!rtc.begin()) {
@@ -46,47 +72,98 @@ void setup() {
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
+
   analogWrite(PIN_BRT, 0);
-//  Serial.begin(57600);
 }
 
 void loop() {
   unsigned long now = millis();
+  bool setDate = digitalRead(PIN_SET_MODE) == LOW;
 
   if (digitalRead(PIN_BTN_HRS) == LOW) {
     if ((now - lastHrsPressed) > 250) {
-      hrs = incrementHrs();
+      if (setDate) {
+        d = incrementDays();
+      } else {
+        hrs = incrementHrs();
+      }
       lastHrsPressed = now;
     }
   } else if (digitalRead(PIN_BTN_MIN) == LOW) {
     if ((now - lastMinsPressed) > 250) {
-      mins = incrementMins();
+      if (setDate) {
+        m = incrementMonths();
+      } else {
+        mins = incrementMins();
+      }
       lastMinsPressed = now;
     }
   } else if (digitalRead(PIN_BTN_SEC) == LOW) {
-    secs = resetSeconds();
+    if (setDate) {
+      if ((now - lastSecsPressed) > 250) {
+        y = incrementYears();
+        lastSecsPressed = now;
+      }
+    } else {
+      secs = resetSeconds();
+    }
   }
 
-  if ((now - lastUpdate) > 100) {
+  if ((now - lastUpdate) > 200) {
     lastUpdate = now;
     DateTime rtcNow = rtc.now();
     hrs = rtcNow.hour();
     mins = rtcNow.minute();
     secs = rtcNow.second();
+    y = rtcNow.year();
+    m = rtcNow.month();
+    d = rtcNow.day();
+
 //    Serial.print(hrs, DEC);
 //    Serial.print(":");
 //    Serial.print(mins, DEC);
 //    Serial.print(":");
 //    Serial.print(secs, DEC);
 //    Serial.println();
+    displayExtra(y, m, d, hrs, mins, secs);
   }
 
   displayTime(hrs, mins, secs);
 
+  //Serial.println(digitalRead(PIN_SET_MODE));
+
   int lightVal = analogRead(PIN_LIGHT_SENS);
   lightVal = constrain(lightVal, 300, 800);
-  lightVal = map(lightVal, 300, 800, 10, 255);
+  lightVal = map(lightVal, 400, 800, 10, 255);
   analogWrite(PIN_BRT, lightVal);
+}
+
+int incrementDays() {
+  DateTime now = rtc.now();
+  int newDay = now.day() + 1;
+  if (newDay > 31) newDay = 1;
+  rtc.adjust(DateTime(now.year(), now.month(), newDay, now.hour(), now.minute(), now.second()));
+  return newDay;
+}
+
+int incrementMonths() {
+  DateTime now = rtc.now();
+  int newMonth = now.month() + 1;
+  if (newMonth > 12) newMonth = 1;
+  rtc.adjust(DateTime(now.year(), newMonth, now.day(), now.hour(), now.minute(), now.second()));
+  return newMonth;
+}
+
+int incrementYears() {
+  DateTime now = rtc.now();
+  int newYear = now.year() + 1;
+  if (newYear > 2100) newYear = 2000;
+  rtc.adjust(DateTime(newYear, now.month(), now.day(), now.hour(), now.minute(), now.second()));
+  return newYear;
 }
 
 int incrementHrs() {
@@ -143,3 +220,17 @@ void displayTime(int hrs, int mins, int secs) {
   }
 }
 
+void displayExtra(int y, int m, int d, int hrs, int mins, int secs) {
+  char timeStr[10],
+       dateStr[20];
+
+  sprintf(timeStr, "%02d:%02d:%02d", hrs, mins, secs);
+  sprintf(dateStr, "%d%s %s, %d", d, ORDS[d-1], MONTHS[m-1], y);
+  Serial.print(d, DEC);
+  Serial.print(ORDS[d-1]);
+  Serial.print(", ");
+  Serial.print(MONTHS[m-1]);
+  Serial.print(" ");
+  Serial.print(y, DEC);
+  Serial.print("\n");
+}
